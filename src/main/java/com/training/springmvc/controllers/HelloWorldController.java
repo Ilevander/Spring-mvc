fc6482a9852236b9e74f4fb8c291df538be6b48f
dell <dell@DESKTOP-4P8U8AR>
2024-03-15 01:18:27 +0100

    properties xml

--- src/main/java/com/training/springmvc/controllers/HelloWorldController.java
+++ src/main/java/com/training/springmvc/controllers/HelloWorldController.java
@@ -1,11 +1,6 @@
 package com.training.springmvc.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;   		
 
-@Controller
-@RequestMapping("/test")
 public class HelloWorldController {
       
 }
