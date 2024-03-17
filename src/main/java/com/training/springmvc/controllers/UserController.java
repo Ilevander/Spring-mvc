2a3149410bce1cab13658805981a8e54c348b1f5
dell <dell@DESKTOP-4P8U8AR>
2024-03-17 02:39:27 +0100

    User Controller

---
+++ src/main/java/com/training/springmvc/controllers/UserController.java
@@ -0,0 +1,81 @@
+package com.training.springmvc.controllers;
+
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.training.springmvc.models.UserModel;
+import com.training.springmvc.models.UserModelValid;
+
+
+@Controller
+@RequestMapping("/test")
+public class UserController {
+	
+	@RequestMapping("/user-registration-form")
+	public String userReegistrationForm(Model model) {
+		model.addAttribute("user", new UserModel());
+		return "userRegistrationForm";
+	}
+	
+	@RequestMapping("/create-user")
+	public String createUser(@ModelAttribute("user") UserModel u) {
+		// store user into the database or perform other business valuable operation
+		System.out.println(u);
+		return "registrationConfirmation";
+	}
+	
+	
+	@RequestMapping("/user-registration-form-with-validation")
+	public String userReegistrationFormWithValidation(Model model) {
+		model.addAttribute("user", new UserModelValid());
+		return "userRegistrationFormWithValidation";
+	}
+	
+	@RequestMapping("/create-user-wtih-validation")
+	public String createUserWithValidation(@Valid @ModelAttribute("user") UserModelValid u, 
+			BindingResult br) {
+		System.out.println("Are there any data binding errors? " + br.hasErrors());
+		
+		
+//		// ================= EXAMPLE of BindingResult API ====================
+//		
+//		List<FieldError> fieldErrors = br.getFieldErrors();
+//		for (FieldError fieldError : fieldErrors) {
+//			System.out.println(fieldError.getDefaultMessage());
+//		}
+//		
+//		// ================= EXAMPLE of Programmatic way to validate object ====================
+//		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+//		Validator validator = factory.getValidator();
+//		Set<ConstraintViolation<UserModelValid>> violations = validator.validate(u);
+//		for (ConstraintViolation<UserModelValid> constraintViolation : violations) {
+//			System.out.println(constraintViolation.getMessage());
+//		}
+		
+		
+		if (br.hasErrors()) {
+			return "userRegistrationFormWithValidation";
+		} else {
+			return "registrationConfirmation";
+		}
+		
+	}
+	
+	// ======= USER REGISTRATION for Spring Security Demo
+	
+	@RequestMapping("/user-registration-form-security-demo")
+	public String userReegistrationSecurityDemo(Model model) {
+		model.addAttribute("user", new UserModelValid());
+		return "userRegistrationSecurityDemo";
+	}
+	
+	
+}
