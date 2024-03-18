35879e1527303a436f87db82ab64be8b80527e88
dell <dell@DESKTOP-4P8U8AR>
2024-03-18 02:09:46 +0100

    Serve Static Resources Controller , static Resources Demo JSP

---
+++ src/main/java/com/training/springmvc/controllers/ServeStaticResourcesController.java
@@ -0,0 +1,14 @@
+package com.training.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class ServeStaticResourcesController {
+
+	@RequestMapping("/serve-static")
+	public String serveStaticResourcesDemo() {
+		return "staticResourcesDemo";
+	}
+}
