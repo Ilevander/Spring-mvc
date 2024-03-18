a5cf596333b21553dcedf2b13519910b806d9daa
dell <dell@DESKTOP-4P8U8AR>
2024-03-18 01:06:28 +0100

    JavaScript code

---
+++ src/main/webapp/WEB-INF/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Testing this Script
+ */
+
+ $(document).ready(function () {
+
+  $('button').click(function() {
+		alert("JS File is Present on the page and function works as expected");
+	});
+
+});
